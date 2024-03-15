ffdffd61365d5169289d532acf936e5a16ec2697
Kirikmelet <troy.dwijanto2@gmail.com>
2024-03-15 01:50:49 -0500

    Initial

--- app/components/header.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestHeader() {
-    return (
-        <nav className="p-1.5 max-h-8 flex flex-row gap-1">
-            <img alt="Troy Judho Dwijanto" className="h-8" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngfind.com%2Fpngs%2Fm%2F610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png&f=1&nofb=1&ipt=04e38e9e627a369e37c94b85e1c0a21f8ff8abbb5bc6eedf8b94f1b610739dbb&ipo=images"></img>
-            <h1 className="text-2xl">Troy Dwijanto</h1>
-        </nav>
-    )
-}
